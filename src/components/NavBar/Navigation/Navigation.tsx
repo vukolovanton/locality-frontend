b523f05ce2757f8ce47c955976137f27b5107764
Anton Vukolov <vukolovanton92@gmail.com>
2021-08-23 23:06:15 +0400

    Dynamically change navigation bar content

--- src/components/NavBar/Navigation/Navigation.tsx
+++ src/components/NavBar/Navigation/Navigation.tsx
@@ -1,31 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import {
+  IS_AUTHENTICATED_LOCATIONS,
+  NOT_AUTHENTICATED_LOCATIONS,
+  USER_FACING_LOCATIONS,
+} from "src/interfaces/constants";
 import "../styles.scss";
 
 const Navigation: React.FC = () => {
+  const rawLocation = useLocation();
+  const currentLocation = rawLocation.pathname.split("/")[1];
+  const isAuthenticated = Boolean(localStorage.getItem("token"));
+
+  const renderNotAuthenticated = () => (
+    <>
+      {NOT_AUTHENTICATED_LOCATIONS.map((location) => (
+        <li key={location.value}>
+          <Link to={`/${location.value}`}>{location.userFacing}</Link>
+        </li>
+      ))}
+    </>
+  );
+
+  const renderIsAuthenticated = () => (
+    <>
+      {IS_AUTHENTICATED_LOCATIONS.map((location) => (
+        <li key={location.value}>
+          <Link to={`/${location.value}`}>{location.userFacing}</Link>
+        </li>
+      ))}
+    </>
+  );
+
   return (
     <nav className="nav__list">
       <li className="nav__listitem">
-        Navigation
+        {USER_FACING_LOCATIONS[currentLocation]}
         <ul className="nav__listitemdrop">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/registration">Registration</Link>
-          </li>
-          <li>
-            <Link to="/announcements">Announcements</Link>
-          </li>
-          <li>
-            <Link to="/documents">Documents</Link>
-          </li>
-          <li>
-            <Link to="/issues">Issues</Link>
-          </li>
+          {isAuthenticated ? renderIsAuthenticated() : renderNotAuthenticated()}
         </ul>
       </li>
     </nav>
--- src/interfaces/constants.ts
+++ src/interfaces/constants.ts
@@ -1,1 +1,41 @@
 export const PAGINATION_LIMIT = 12;
+
+const HOME = "";
+const LOGIN = "login";
+const REGISTRATION = "registration";
+const ANNOUNCEMENTS = "announcements";
+const ISSUES = "issues";
+
+export const USER_FACING_LOCATIONS: any = {
+  [LOGIN]: "Login",
+  [REGISTRATION]: "Registration",
+  [HOME]: "Home",
+  [ANNOUNCEMENTS]: "Announcements",
+  [ISSUES]: "Issues",
+};
+
+export const IS_AUTHENTICATED_LOCATIONS = [
+  {
+    value: HOME,
+    userFacing: USER_FACING_LOCATIONS[HOME],
+  },
+  {
+    value: ANNOUNCEMENTS,
+    userFacing: USER_FACING_LOCATIONS[ANNOUNCEMENTS],
+  },
+  {
+    value: ISSUES,
+    userFacing: USER_FACING_LOCATIONS[ISSUES],
+  },
+];
+
+export const NOT_AUTHENTICATED_LOCATIONS = [
+  {
+    value: LOGIN,
+    userFacing: USER_FACING_LOCATIONS[LOGIN],
+  },
+  {
+    value: REGISTRATION,
+    userFacing: USER_FACING_LOCATIONS[REGISTRATION],
+  },
+];
